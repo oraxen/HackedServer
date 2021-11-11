a9437a10c23082a5ad246173b897fd8315fff8f3
Thomas Marchand <me@thomas-marchand.com>
2021-11-11 18:20:19 +0100

    introduce bungee update

--- hackedserver-bungeecord/src/main/java/org/hackedserver/bungee/HackedServerPlugin.java
+++ hackedserver-bungeecord/src/main/java/org/hackedserver/bungee/HackedServerPlugin.java
@@ -1,4 +1,33 @@
 package org.hackedserver.bungee;
 
-public class HackedServerPlugin {
+import net.kyori.adventure.platform.bungeecord.BungeeAudiences;
+import net.md_5.bungee.api.plugin.Plugin;
+import org.hackedserver.bungee.listeners.CustomPayloadListener;
+import org.hackedserver.bungee.listeners.HackedPlayerListeners;
+import org.hackedserver.bungee.logs.Logs;
+import org.hackedserver.core.config.ConfigsManager;
+
+public class HackedServerPlugin extends Plugin {
+
+    private BungeeAudiences audiences;
+    private static HackedServerPlugin instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        audiences = BungeeAudiences.create(this);
+        Logs.onEnable(getLogger(), audiences);
+        ConfigsManager.init(getLogger(), getDataFolder());
+
+        this.getProxy().getPluginManager().registerListener(this, new HackedPlayerListeners());
+        this.getProxy().getPluginManager().registerListener(this, new CustomPayloadListener());
+    }
+
+    public static HackedServerPlugin get() {
+        return instance;
+    }
+
+    public BungeeAudiences getAudiences() {
+        return audiences;
+    }
 }
