8ea110238e87f0efa79d73e53f72ea33768411a1
Thomas Marchand <thomas.marchand@tuta.io>
2025-01-05 09:52:17 +0000

    fix: commands api version incompatible with 1.21.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("java")
 }
 
-val pluginVersion = "3.5.0"
+val pluginVersion = "3.5.1"
 
 allprojects {
     apply(plugin = "idea")
@@ -69,7 +69,7 @@ project(":hackedserver-spigot") {
         compileOnly("io.netty:netty-all:4.1.68.Final")
         compileOnly(project(path = ":hackedserver-core", configuration = "shadow"))
 
-        implementation("dev.jorel:commandapi-bukkit-shade:9.6.1")
+        implementation("dev.jorel:commandapi-bukkit-shade:9.7.0")
         implementation("net.kyori:adventure-platform-bukkit:4.3.0")
         implementation("org.bstats:bstats-bukkit:3.1.0")
     }
